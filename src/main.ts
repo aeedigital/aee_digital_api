5a061e7fd606f0ff3073fab1f7a9664a37cc9eb9
bruno.duartec <bruno.duartec@gmail.com>
2023-11-22 19:20:49 -0300

    add logger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as packageInfo from '../package.json';
+import { WinstonLogger } from './services/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: new WinstonLogger(),
+  });
 
   const config = new DocumentBuilder()
     .setTitle(packageInfo.name)
@@ -14,8 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   const port = process.env.PORT || 5000;
-
-  console.log('PORT', port);
   await app.listen(port);
 }
 bootstrap();
